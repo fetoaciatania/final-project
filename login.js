c3c22586218c8b1aa2984d3adb7853761e688012
AireenLie <AireenVierlie@outlook.com>
2022-11-15 21:32:02 +0700

    updated

--- login.js
+++ login.js
@@ -1,40 +1,17 @@
 const loginButton = document.getElementById("login-form-submit");
+loginButton.onclick = checkUserLogin
 
-const elFormSignIn = document.getElementById('login-form')
-elFormSignIn.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(elFormSignIn)
-    const values = Object.fromEntries(formData)
-    
-    const { uname, psw } = values
-    console.log (uname, psw, 'ini username dan password')
-    let url = 'https://635d2135fc2595be26538938.mockapi.io/GirlsZone/eheheh/GirlsZoneUser'
-  
-  fetch(url, {
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(values)
-  })
-  .then((response)=> response.json())
-  .then((value)=> {
-    console.log(value)
-  })
-  .catch((error) =>{
-    console.log(error)
-  })
-  
-    if (!uname) {
-      alert(`Username/email can't be empty !`)
-    } else if (!psw) {
-      alert(`Password can't be empty !`)
-    } else {
-      const userLogin = {
-        username,
-        Password,
+function checkUserLogin () {
+      console.log('test')
+      const getUser = localStorage.getItem('uname')
+      if (getUser) {
+        // jika sudah ada yang login
+        const userLogin = JSON.parse(getUser)
+        // console.log(userLogin)
+        const username = userLogin.username
+        console.log(username, 'username yg login')
+        window.location.href="./homepage.html"
       }
-      const userLoginStr = JSON.stringify(userLogin)
-      localStorage.setItem('user', userLoginStr)
     }
-  })
+
+    checkUserLogin()
